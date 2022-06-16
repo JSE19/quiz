4c4632427d633ec6a37876f468be20d3a6ccfa17
JSE <joysundayegbala@gmail.com>
2022-06-16 21:24:37 +0100

    First Commit

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+    define('SITEURL','http://localhost:8080/grafilexams/');
+    define('LOCALHOST','localhost');
+    define('USERNAME','joy');
+    define('PASSWORD','joy123');
+    define('DBNAME','quizapp');
+?>
---
+++ box/footer.php
@@ -0,0 +1,11 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    <a href="https://www.grafilcomputers.com/" title="Grafil Computers">Anambra Teacher Recruitment</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="https://www.grafilcomputers.com" title="Vijay Thapa" target="_blank">Grafil Computers</a>.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
